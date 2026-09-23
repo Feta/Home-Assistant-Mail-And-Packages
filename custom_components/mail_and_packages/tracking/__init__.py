2a34697934a842f1847583ad6229710216f764e3
Feta <38106169+Feta@users.noreply.github.com>
2026-09-23 16:11:06 -0400

    feat: export tracking forwarding helpers

--- custom_components/mail_and_packages/tracking/__init__.py
+++ custom_components/mail_and_packages/tracking/__init__.py
@@ -1,5 +1,11 @@
 """Package tracking helpers."""
 
+from .forwarding import ForwardingResult, async_forward_pending_to_seventeentrack
 from .registry import STATUS_RANK, PackageRegistry
 
-__all__ = ["STATUS_RANK", "PackageRegistry"]
+__all__ = [
+    "STATUS_RANK",
+    "ForwardingResult",
+    "PackageRegistry",
+    "async_forward_pending_to_seventeentrack",
+]
