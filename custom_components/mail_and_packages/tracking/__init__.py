09df303efa7af81bdab0132e694119640f3c01a5
Feta <38106169+Feta@users.noreply.github.com>
2026-09-24 00:03:56 -0400

    feat: export 17TRACK snapshot helper

--- custom_components/mail_and_packages/tracking/__init__.py
+++ custom_components/mail_and_packages/tracking/__init__.py
@@ -1,6 +1,11 @@
 """Package tracking helpers."""
 
-from .forwarding import ForwardingResult, async_forward_pending_to_seventeentrack
+from .forwarding import (
+    ForwardingResult,
+    ProviderSnapshot,
+    async_forward_pending_to_seventeentrack,
+    async_get_seventeentrack_snapshot,
+)
 from .registry import STATUS_RANK, PackageRegistry
 from .scanner import UniversalScanResult, async_scan_tracking_emails
 
@@ -8,7 +13,9 @@ __all__ = [
     "STATUS_RANK",
     "ForwardingResult",
     "PackageRegistry",
+    "ProviderSnapshot",
     "UniversalScanResult",
     "async_forward_pending_to_seventeentrack",
+    "async_get_seventeentrack_snapshot",
     "async_scan_tracking_emails",
 ]
