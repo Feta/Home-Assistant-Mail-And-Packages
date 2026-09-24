d104309fe4097908cc2cc19fe484aff0926c9979
Feta <38106169+Feta@users.noreply.github.com>
2026-09-23 22:27:54 -0400

    feat: export universal tracking scanner

--- custom_components/mail_and_packages/tracking/__init__.py
+++ custom_components/mail_and_packages/tracking/__init__.py
@@ -2,10 +2,13 @@
 
 from .forwarding import ForwardingResult, async_forward_pending_to_seventeentrack
 from .registry import STATUS_RANK, PackageRegistry
+from .scanner import UniversalScanResult, async_scan_tracking_emails
 
 __all__ = [
     "STATUS_RANK",
     "ForwardingResult",
     "PackageRegistry",
+    "UniversalScanResult",
     "async_forward_pending_to_seventeentrack",
+    "async_scan_tracking_emails",
 ]
